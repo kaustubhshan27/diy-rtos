26509dcfd1dfbb337b824079f23158959214fb87
Kaustubh Shankar <kaustubhshan@gmail.com>
2024-05-02 13:07:19 -0400

    kernel initialization code added

---
+++ Core/Src/os_kernel/os_kernel.h
@@ -0,0 +1,14 @@
+/*
+ * os_kernel.h
+ *
+ *  Created on: May 2, 2024
+ *      Author: kaustubh
+ */
+
+#ifndef SRC_OS_KERNEL_OS_KERNEL_H_
+#define SRC_OS_KERNEL_OS_KERNEL_H_
+
+#include "stdint.h"
+#include "stm32f4xx.h"
+
+#endif /* SRC_OS_KERNEL_OS_KERNEL_H_ */
